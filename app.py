0ab8dd57698c864a663551676db315ad6f11ea3a
TYCheng31 <littleyunkevin@gmail.com>
2025-09-16 14:55:40 +0800

    v1.0.0

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 import csv
+from datetime import datetime  # 引入 datetime 模組
 from process_data import process_data  # 引入你剛定義的函式
 
 app = Flask(__name__)
@@ -26,16 +27,20 @@ def index():
     # 呼叫處理程式並取得題目名稱
     task_names = process_data()
 
+    # 處理 POST 請求，篩選 CSV 資料
     if request.method == 'POST':
         print("POST request received.")  # 檢查是否進入 POST 請求
         selected_tasks = request.form.getlist('task_names')  # 取得所有勾選的題目名稱
         print(f"Selected tasks: {selected_tasks}")  # 印出選擇的題目名稱
 
         if selected_tasks:
+            # 根據選擇的任務過濾 CSV 資料，假設任務名稱在 CSV 的第三列（索引 2）
             csv_data = [row for row in csv_data if row[2] in selected_tasks]
+    
+    # 取得當前時間並格式化為 YYYY-MM-DD HH:MM:SS 格式
+    last_updated_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-
-    return render_template('index.html', csv_data=csv_data, task_names=task_names, selected_tasks=selected_tasks)
+    return render_template('index.html', csv_data=csv_data, task_names=task_names, selected_tasks=selected_tasks, last_updated_time=last_updated_time)
 
 @app.route('/run', methods=['POST'])
 def run_script():
